8bc77e009c36d10c532335657f03d48f49ffd655
Seo MinSeok <minsoku03@inha.edu>
2023-12-19 23:15:06 +0900

    fix: css 추가

--- src/app/(afterLogin)/home/layout.tsx
+++ src/app/(afterLogin)/home/layout.tsx
@@ -1,8 +1,9 @@
 export default async function HomeLayout ({children}) {
-	console.log(children);
-	console.log(2);
+
 	return (
-		<div>{children}
-			홈레이아웃</div>
+		<div>
+			{/*{children}*/}
+			홈레이아웃
+		</div>
 	)
-} 
+}
